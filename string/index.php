38fbd8e0c7d4b0540cebc96d1e6d5a51b79d1cc0
LaJannet <lajannetwork@gmail.com>
2022-10-02 15:44:33 +0300

    Initial commit

--- string/index.php
+++ string/index.php
@@ -1,4 +1,5 @@
 <?php
-$str = "[    34  555   8 9 9]";
-$res = str_replace(" ", "", $str);
-print $res;
+$str = "[ 34 555 8 9 9]";
+print(str_replace(" ", "", $str));
+// $res = str_replace(" ", "", $str);
+// print $res;
